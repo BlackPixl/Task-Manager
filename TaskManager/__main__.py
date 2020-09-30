382dc1d33a222e796a921d2c209ac2899325e538
BlackPixl <javierdarioechavarria@gmail.com>
2020-09-30 18:22:10 -0500

    Created basic skeleton.

--- TaskManager/__main__.py
+++ TaskManager/__main__.py
@@ -1,21 +1,14 @@
 import arguments
-
-
-def list_processes():
-    return 'TestPrintProcesses'
-
-
-def create_process(process):
-    return process
-
+import processes
 
 args = arguments.args()
 
 print(args)
 
 new_process = args.get("new")
+
 if new_process:
-    print(create_process(new_process))
+    print(processes.create_process(new_process))
 
 if args.pop("list"):
-    print(list_processes())
+    print(processes.list_processes())
---
+++ TaskManager/processes.py
@@ -0,0 +1,6 @@
+def list_processes():
+    return 'TestPrintProcesses'
+
+
+def create_process(process):
+    return process
